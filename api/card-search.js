16c7e44591ccd2c8d023ca92035e547c68d4a17b
f6t7zw67pw-dotcom <f6t7zw67pw@privaterelay.appleid.com>
2026-07-04 01:14:03 +0200

    Normalize foreign names before card search

--- api/card-search.js
+++ api/card-search.js
@@ -1,1 +1,9 @@
-export { default } from './cards/search.js';
+import searchHandler from './cards/search.js';
+import { enrichForeignNameInput } from './_i18n-name.js';
+
+export default async function handler(req, res) {
+  if (req.method === 'POST' && req.body && typeof req.body === 'object') {
+    req.body = await enrichForeignNameInput(req.body);
+  }
+  return searchHandler(req, res);
+}
