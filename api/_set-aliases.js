a8be6ed90920aeef455670ea828254703f285923
f6t7zw67pw-dotcom <f6t7zw67pw@privaterelay.appleid.com>
2026-07-03 21:50:48 +0200

    Add expanded Japanese set aliases

---
+++ api/_set-aliases.js
@@ -0,0 +1,127 @@
+export const EXTRA_SET_ALIASES = {
+  BLK: ['Black Bolt', 'Black-Bolt', 'Schwarze Blitze', 'SV11B', 'sv11b'],
+  WHT: ['White Flare', 'White-Flare', 'Weiße Flammen', 'Weisse Flammen', 'SV11W', 'sv11w'],
+  SV11B: ['Black Bolt', 'Black-Bolt', 'BLK', 'Schwarze Blitze'],
+  SV11W: ['White Flare', 'White-Flare', 'WHT', 'Weiße Flammen', 'Weisse Flammen'],
+  SV10: ['The Glory of Team Rocket', 'Glory of Team Rocket', 'Team Rocket', 'Rocket-Dan no Eiko'],
+  SV9A: ['Heat Wave Arena', 'Heat-Wave-Arena', 'Nekketsu Arena'],
+  SV9: ['Battle Partners', 'Battle-Partners', 'Battle Partners JP'],
+  SV8A: ['Terastal Festival ex', 'Terastal-Festival-ex', 'Terastal Festival', 'PRE', 'Prismatic Evolutions'],
+  SV8: ['Super Electric Breaker', 'Super-Electric-Breaker', 'Surging Sparks', 'SSP'],
+  SV7A: ['Paradise Dragona', 'Paradise-Dragona'],
+  SV7: ['Stellar Miracle', 'Stellar-Miracle', 'Stellar Crown', 'SCR'],
+  SV6A: ['Night Wanderer', 'Night-Wanderer', 'Shrouded Fable', 'SFA'],
+  SV6: ['Mask of Change', 'Mask-of-Change', 'Twilight Masquerade', 'TWM'],
+  SV5A: ['Crimson Haze', 'Crimson-Haze'],
+  SV5M: ['Cyber Judge', 'Cyber-Judge', 'Temporal Forces', 'TEF'],
+  SV5K: ['Wild Force', 'Wild-Force', 'Temporal Forces', 'TEF'],
+  SV4A: ['Shiny Treasure ex', 'Shiny-Treasure-ex', 'Paldean Fates', 'PAF'],
+  SV4M: ['Future Flash', 'Future-Flash', 'Paradox Rift', 'PAR'],
+  SV4K: ['Ancient Roar', 'Ancient-Roar', 'Paradox Rift', 'PAR'],
+  SV3A: ['Raging Surf', 'Raging-Surf'],
+  SV3: ['Ruler of the Black Flame', 'Ruler-of-the-Black-Flame', 'Obsidian Flames', 'OBF'],
+  SV2A: ['Pokemon Card 151', 'Pokemon-Card-151', 'Pokemon 151', 'Pokémon Card 151', 'MEW'],
+  SV2P: ['Snow Hazard', 'Snow-Hazard', 'Paldea Evolved', 'PAL'],
+  SV2D: ['Clay Burst', 'Clay-Burst', 'Paldea Evolved', 'PAL'],
+  SV1A: ['Triplet Beat', 'Triplet-Beat'],
+  SV1S: ['Scarlet ex', 'Scarlet-ex', 'Scarlet and Violet', 'SVI'],
+  SV1V: ['Violet ex', 'Violet-ex', 'Scarlet and Violet', 'SVI'],
+  S12A: ['VSTAR Universe', 'VSTAR-Universe', 'Crown Zenith', 'CRZ'],
+  S12: ['Paradigm Trigger', 'Paradigm-Trigger', 'Silver Tempest', 'SIT'],
+  S11A: ['Incandescent Arcana', 'Incandescent-Arcana'],
+  S11: ['Lost Abyss', 'Lost-Abyss', 'Lost Origin', 'LOR'],
+  S10B: ['Pokemon GO', 'Pokémon GO', 'Pokemon-Go', 'PGO'],
+  S10A: ['Dark Phantasma', 'Dark-Phantasma'],
+  S10P: ['Space Juggler', 'Space-Juggler', 'Astral Radiance', 'ASR'],
+  S10D: ['Time Gazer', 'Time-Gazer', 'Astral Radiance', 'ASR'],
+  S9A: ['Battle Region', 'Battle-Region'],
+  S9: ['Star Birth', 'Star-Birth', 'Brilliant Stars', 'BRS'],
+  S8B: ['VMAX Climax', 'VMAX-Climax'],
+  S8A: ['25th Anniversary Collection', '25th-Anniversary-Collection', 'Celebrations', 'CEL'],
+  S8: ['Fusion Arts', 'Fusion-Arts', 'Fusion Strike', 'FST'],
+  S7R: ['Blue Sky Stream', 'Blue-Sky-Stream', 'Evolving Skies', 'EVS'],
+  S7D: ['Skyscraping Perfection', 'Skyscraping-Perfection', 'Evolving Skies', 'EVS'],
+  S6A: ['Eevee Heroes', 'Eevee-Heroes', 'Evolving Skies', 'EVS'],
+  S6K: ['Jet-Black Spirit', 'Jet Black Spirit', 'Chilling Reign', 'CRE'],
+  S6H: ['Silver Lance', 'Silver-Lance', 'Chilling Reign', 'CRE'],
+  S5A: ['Matchless Fighters', 'Matchless-Fighters', 'Chilling Reign', 'CRE'],
+  S5R: ['Rapid Strike Master', 'Rapid-Strike-Master', 'Battle Styles', 'BST'],
+  S5I: ['Single Strike Master', 'Single-Strike-Master', 'Battle Styles', 'BST'],
+  S4A: ['Shiny Star V', 'Shiny-Star-V', 'Shining Fates', 'SHF'],
+  S4: ['Amazing Volt Tackle', 'Amazing-Volt-Tackle', 'Vivid Voltage', 'VIV'],
+  S3A: ['Legendary Heartbeat', 'Legendary-Heartbeat', 'Vivid Voltage', 'VIV'],
+  S3: ['Infinity Zone', 'Infinity-Zone', 'Darkness Ablaze', 'DAA'],
+  S2A: ['Explosive Walker', 'Explosive-Walker', 'Darkness Ablaze', 'DAA'],
+  S2: ['Rebellion Crash', 'Rebellion-Crash', 'Rebel Clash', 'RCL'],
+  S1A: ['VMAX Rising', 'VMAX-Rising'],
+  S1W: ['Sword', 'Sword Collection', 'Sword & Shield', 'SSH'],
+  S1H: ['Shield', 'Shield Collection', 'Sword & Shield', 'SSH'],
+  SM12A: ['Tag All Stars', 'Tag-All-Stars'],
+  SM12: ['Alter Genesis', 'Alter-Genesis', 'Cosmic Eclipse', 'CEC'],
+  SM11B: ['Dream League', 'Dream-League', 'Cosmic Eclipse', 'CEC'],
+  SM11A: ['Remix Bout', 'Remix-Bout'],
+  SM11: ['Miracle Twin', 'Miracle-Twin', 'Unified Minds', 'UNM'],
+  SM10B: ['Sky Legend', 'Sky-Legend'],
+  SM10A: ['GG End', 'GG-End'],
+  SM10: ['Double Blaze', 'Double-Blaze', 'Unbroken Bonds', 'UNB'],
+  SM9B: ['Full Metal Wall', 'Full-Metal-Wall'],
+  SM9A: ['Night Unison', 'Night-Unison'],
+  SM9: ['Tag Bolt', 'Tag-Bolt', 'Team Up', 'TEU'],
+  SM8B: ['GX Ultra Shiny', 'GX-Ultra-Shiny'],
+  SM8A: ['Dark Order', 'Dark-Order'],
+  SM8: ['Super-Burst Impact', 'Super Burst Impact', 'Lost Thunder', 'LOT'],
+  SM7B: ['Fairy Rise', 'Fairy-Rise'],
+  SM7A: ['Thunderclap Spark', 'Thunderclap-Spark'],
+  SM7: ['Charisma of the Wrecked Sky', 'Celestial Storm', 'CES'],
+  SM6B: ['Champion Road', 'Champion-Road'],
+  SM6A: ['Dragon Storm', 'Dragon-Storm'],
+  SM6: ['Forbidden Light', 'FLI'],
+  SM5M: ['Ultra Moon', 'Ultra-Moon', 'Ultra Prism', 'UPR'],
+  SM5S: ['Ultra Sun', 'Ultra-Sun', 'Ultra Prism', 'UPR'],
+  SM4A: ['GX Battle Boost', 'GX-Battle-Boost'],
+  SM4S: ['Awakened Heroes', 'Awakened-Heroes', 'Crimson Invasion', 'CIN'],
+  SM4P: ['Ultradimensional Beasts', 'Ultra-Dimensional Beasts', 'Crimson Invasion', 'CIN'],
+  SM3H: ['Darkness that Consumes Light', 'Burning Shadows', 'BUS'],
+  SM3N: ['Light Consuming Darkness', 'Burning Shadows', 'BUS'],
+  SM2K: ['Islands Await You', 'Guardians Rising', 'GRI'],
+  SM2L: ['Alolan Moonlight', 'Guardians Rising', 'GRI'],
+  SM1M: ['Collection Moon', 'Sun & Moon', 'SUM'],
+  SM1S: ['Collection Sun', 'Sun & Moon', 'SUM']
+};
+
+function norm(value) {
+  return String(value || '').trim().toUpperCase().replace(/\s+/g, '');
+}
+
+function fold(value) {
+  return String(value || '')
+    .trim()
+    .toLowerCase()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/[^a-z0-9]+/g, ' ')
+    .replace(/\s+/g, ' ')
+    .trim();
+}
+
+export function extraSetAliasesFor(code) {
+  return EXTRA_SET_ALIASES[norm(code)] || [];
+}
+
+export function expandedSetCodeVariants(...values) {
+  const variants = new Set();
+  const foldedValues = values.map(fold).filter(Boolean);
+  for (const value of values) {
+    const code = norm(value);
+    if (code) variants.add(code);
+    for (const alias of extraSetAliasesFor(code)) variants.add(norm(alias));
+  }
+  for (const [code, aliases] of Object.entries(EXTRA_SET_ALIASES)) {
+    const foldedAliases = [code, ...aliases].map(fold);
+    if (foldedValues.some((value) => foldedAliases.some((alias) => alias && (alias === value || alias.includes(value) || value.includes(alias))))) {
+      variants.add(code);
+      aliases.forEach((alias) => variants.add(norm(alias)));
+    }
+  }
+  return Array.from(variants).filter(Boolean);
+}
